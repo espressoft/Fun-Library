91c93bdc7cb805bb21660d051ae0c8929af0dc7e
cs soon <cs.soon@espressoft.com.my>
2015-04-11 11:59:34 +0800

    Initial Commit

---
+++ FunLibrary/FunLibrary.h
@@ -0,0 +1,13 @@
+//
+//  FunLibrary.h
+//  FunLibrary
+//
+//  Created by CS Soon on 4/11/15.
+//  Copyright (c) 2015 Espressoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FunLibrary : NSObject
+
+@end
---
+++ FunLibrary/FunLibrary.m
@@ -0,0 +1,13 @@
+//
+//  FunLibrary.m
+//  FunLibrary
+//
+//  Created by CS Soon on 4/11/15.
+//  Copyright (c) 2015 Espressoft. All rights reserved.
+//
+
+#import "FunLibrary.h"
+
+@implementation FunLibrary
+
+@end
